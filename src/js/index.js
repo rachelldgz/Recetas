ccc9518bf4296aa2bb7595c6a4e9318f060b4977
Kify <klores.valona@gmail.com>
2021-08-15 15:56:42 -0500

    modal resonving conflicts

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import "../css/styles.css";
+import "../js/script.js";
 import "../assets/vendors/bootstrap/css/bootstrap.min.css"
 import "../assets/vendors/bootstrap/js/bootstrap.bundle.js"
+
 
 /* Modal JS */
 
---
+++ src/js/script.js
@@ -0,0 +1,78 @@
+const searchBtn = document.getElementById('btn-search');
+const mealList = document.getElementById('meal');
+const randomBtn = document.getElementById('btn-random');
+
+
+// event listeners
+searchBtn.addEventListener('click', getMealList);
+randomBtn.addEventListener('click', () => {
+    searchInput.value = " ";
+    getRandomMeallist();
+})
+
+
+function getMealList() {
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    console.log(searchInputTxt);
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+                 <div class="card shadow p-3 bg-white rounded"  id="${meal.idMeal}">
+                         <img class="imagen-comida" src ="${meal.strMealThumb}" alt="imagen_comida">
+                       <div class="card-body">
+                            <h5 class="card-title">${meal.strMeal}</h5>
+                  </div>
+                     <div class="card-footer">
+                            <p class="card-text" id="getMeal"><small class="text-muted"> <a href=""> Get Recipe →</a></small>
+                         </p>
+                       </div>
+                    </div>
+                `;
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
+
+            mealList.innerHTML = html;
+        });
+
+}
+
+function getRandomMeallist() {
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let html = "";
+            if (data.meals) {
+                data.meals.forEach(meal => {
+                    html += `
+             <div class="card justify-content-md-center shadow p-3 bg-white rounded"  id="${meal.idMeal}">
+                     <img class="imagen-comida" src ="${meal.strMealThumb}" alt="imagen_comida">
+                   <div class="card-body">
+                        <h5 class="card-title">${meal.strMeal}</h5>
+              </div>
+                 <div class="card-footer">
+                        <p class="card-text" id="getMeal"><small class="text-muted"> <a href=""> Get Recipe →</a></small>
+                     </p>
+                   </div>
+                </div>
+            `;
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
+            mealList.innerHTML = html;
+        });
+
+}
