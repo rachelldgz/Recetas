6127188b0979eadd08bad0a1b668730d6169573a
Kify <klores.valona@gmail.com>
2021-08-15 14:34:57 -0500

    modal html, css y js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,16 @@
 import "../css/styles.css";
 import "../assets/vendors/bootstrap/css/bootstrap.min.css"
 import "../assets/vendors/bootstrap/js/bootstrap.bundle.js"
+
+/* Modal JS */
+
+const ourModal = document.getElementById('ourModal');
+const modalButton = document.getElementById('modal-btn');
+const close = document.getElementsByClassName("close")[0];
+
+modalButton.addEventListener('click', function() {
+    ourModal.style.display = "block;";
+  })
+  close.addEventListener('click', function() {
+    ourModal.style.display = "none;";
+  })
